d6a19a12090b6f424c005886c2a513d7ca11fe78
Alexis Schad <schadoc_alex@hotmail.fr>
2014-11-05 21:42:37 +0100

    Ajout du ratio PR open/closed + fonction scraper_repo

--- scraper.php
+++ scraper.php
@@ -11,27 +11,50 @@ require_once('simple_html_dom/simple_html_dom.php');
 	</head>
 	<body>
 <?php
-
-/********************************
-		ISSUES
-*********************************/
-	$html = file_get_html('http://github.com/'.$projet.'/issues');
-	
-	// ratio issues open/closed
-	$i = 0;
-	foreach ($html->find('div.table-list-header-toggle a.button-link') as $temp)
+	function scraper_repo($name)
 	{
-		$values[$i] = intval(trim(htmlspecialchars_decode($temp->plaintext)));
-		$i++;
+		/********************************
+			ISSUES
+		*********************************/
+		$html = file_get_html('http://github.com/'.$name.'/issues');
+
+		// ratio issues open/closed
+		$i = 0;
+		foreach ($html->find('div.table-list-header-toggle a.button-link') as $temp)
+		{
+			$values[$i] = intval(trim(htmlspecialchars_decode($temp->plaintext)));
+			$i++;
+		}
+		$nb_issues_open = intval($values[0]);
+		$nb_issues_closed = intval($values[1]);
+		$ratio = -1;
+		if($nb_issues_closed > 0)
+			$ratio_issues = $nb_issues_open / $nb_issues_closed;
+
+		/********************************
+			PULL_REQUEST
+		*********************************/
+		$html = file_get_html('http://github.com/'.$name.'/pulls');
+
+		// ratio PR open/closed
+		$i = 0;
+		foreach ($html->find('div.table-list-header-toggle a.button-link') as $temp)
+		{
+			$values[$i] = intval(trim(htmlspecialchars_decode($temp->plaintext)));
+			$i++;
+		}
+		$nb_PR_open = intval($values[0]);
+		$nb_PR_closed = intval($values[1]);
+		$ratio = -1;
+		if($nb_PR_closed > 0)
+			$ratio_PR = $nb_PR_open / $nb_PR_closed;
+
+		return "$ratio_issues;$ratio_PR";
 	}
-	$nb_issues_opened = intval($values[0]);
-	$nb_issues_closed = intval($values[1]);
-	$ratio = -1;
-	if($nb_issues_closed > 0)
-		$ratio = $nb_issues_opened / $nb_issues_closed;
 ?>
-	Issues opened : <?= $nb_issues_opened ?><br />
-	Issues closed : <?= $nb_issues_closed ?><br />
-	ratio issues : <?= $ratio ?><br />
+Ratio issues open/closed ;Ratio PR open/closed
+<br/>
+	<?= scraper_repo("gephi/gephi") ?>
+<br />
 	</body>
 </html>
